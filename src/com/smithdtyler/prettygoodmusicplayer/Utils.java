c08898e7db9738ea6a4666ad230c5e36ae5cf330
Tyler Smith <tylerhesthedude@gmail.com>
2014-08-31 09:33:33 -0500

    improved selection of potential folders

--- src/com/smithdtyler/prettygoodmusicplayer/Utils.java
+++ src/com/smithdtyler/prettygoodmusicplayer/Utils.java
@@ -158,20 +158,25 @@ public class Utils {
 	// TODO use a file filter?
 	/**
 	 * Returns a list of viable directories under the parent file.
+	 * 
 	 * @param parent
 	 * @return A list of viable files. An empty list is returned if none exist.
 	 */
 	static List<File> getPotentialSubDirectories(File parent) {
 		List<File> list = new ArrayList<File>();
-		if (parent.isDirectory()) {
-			if (!parent.isHidden()) {
-				File[] files = parent.listFiles();
-				if (files != null && files.length > 0) {
-					for (File f : files) {
-						list.add(f);
+		if (parent.isDirectory() && !parent.isHidden()) {
+			File[] files = parent.listFiles();
+			if (files != null && files.length > 0) {
+				for (File f : files) {
+					// If the subfolder is non-empty, add it
+					if (f.isDirectory() && !f.isHidden()
+							&& !f.getName().startsWith(".")) {
+						if (f.listFiles() != null && f.listFiles().length > 0) {
+							list.add(f);
+						}
 					}
-					return list;
 				}
+				return list;
 			}
 		}
 		return list;
