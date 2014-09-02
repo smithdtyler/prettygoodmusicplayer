8cecbe5dc2744bf9476d28de62c1241b42922d10
Tyler Smith <tylerhesthedude@gmail.com>
2014-09-01 19:53:51 -0500

    fixed focus gained bug

--- src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
+++ src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
@@ -70,7 +70,7 @@ public class MusicPlaybackService extends Service {
 	static final int MSG_STOP_SERVICE = 19;
 
 	public enum PlaybackState {
-		PLAYING, PAUSED
+		PLAYING, PAUSED, UNKNOWN
 	}
 
 	static final String PRETTY_SONG_NAME = "PRETTY_SONG_NAME";
@@ -537,11 +537,18 @@ public class MusicPlaybackService extends Service {
 
 	private class PrettyGoodAudioFocusChangeListener implements
 			AudioManager.OnAudioFocusChangeListener {
+		
+		private PlaybackState stateOnFocusLoss = PlaybackState.UNKNOWN;
 
 		public void onAudioFocusChange(int focusChange) {
 			Log.w(TAG, "Focus change received " + focusChange);
 			if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
 				Log.i(TAG, "AUDIOFOCUS_LOSS_TRANSIENT");
+				if(mp.isPlaying()){
+					stateOnFocusLoss = PlaybackState.PLAYING;
+				} else {
+					stateOnFocusLoss = PlaybackState.PAUSED;
+				}
 				pause();
 				MusicPlaybackService.this.audioFocusLossTime  = System.currentTimeMillis();
 				// Pause playback
@@ -549,27 +556,37 @@ public class MusicPlaybackService extends Service {
 				Log.i(TAG, "AUDIOFOCUS_GAIN");
 				// If it's been less than 20 seconds, resume playback
 				long curr = System.currentTimeMillis();
-				if((curr - MusicPlaybackService.this.audioFocusLossTime) < 30000){
+				if(((curr - MusicPlaybackService.this.audioFocusLossTime) < 30000) && stateOnFocusLoss == PlaybackState.PLAYING){
 					play();
 				} else {
-					Log.i(TAG, "It's been more than 30 seconds, don't auto-play");
+					Log.i(TAG, "It's been more than 30 seconds or we were paused, don't auto-play");
 				}
 			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
 				Log.i(TAG, "AUDIOFOCUS_LOSS");
+				if(mp.isPlaying()){
+					stateOnFocusLoss = PlaybackState.PLAYING;
+				} else {
+					stateOnFocusLoss = PlaybackState.PAUSED;
+				}
 				pause();
 				MusicPlaybackService.this.audioFocusLossTime  = System.currentTimeMillis();
 				// Stop playback
 			} else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
 				Log.i(TAG, "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
 				MusicPlaybackService.this.audioFocusLossTime  = System.currentTimeMillis();
+				if(mp.isPlaying()){
+					stateOnFocusLoss = PlaybackState.PLAYING;
+				} else {
+					stateOnFocusLoss = PlaybackState.PAUSED;
+				}
 				pause();
 			} else if (focusChange == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK) {
 				Log.i(TAG, "AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK");
 				long curr = System.currentTimeMillis();
-				if((curr - MusicPlaybackService.this.audioFocusLossTime) < 30000){
+				if(((curr - MusicPlaybackService.this.audioFocusLossTime) < 30000) && stateOnFocusLoss == PlaybackState.PLAYING){
 					play();
 				} else {
-					Log.i(TAG, "It's been more than 30 seconds, don't auto-play");
+					Log.i(TAG, "It's been more than 30 seconds or we were paused, don't auto-play");
 				}
 			}
 		}
