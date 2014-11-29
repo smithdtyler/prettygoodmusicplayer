5230e413bae5df9155a826e9fd0b05a21394a7ae
smith.d.tyler <smith.d.tyler@gmail.com>
2014-11-29 06:47:49 -0600

    updated to allow seeking while paused

--- src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
+++ src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
@@ -487,6 +487,14 @@ public class MusicPlaybackService extends Service {
 	private synchronized void jumpTo(int position){
 		if(mp.isPlaying()){
 			mp.seekTo(position);
+		} else {
+			// if we're paused but initialized, try to seek
+			try{
+				mp.seekTo(position);
+				lastPosition = mp.getCurrentPosition();
+			} catch (Exception e){
+				Log.w(TAG, "Unable to seek to position, file may not have been loaded");
+			}
 		}
 	}
 
