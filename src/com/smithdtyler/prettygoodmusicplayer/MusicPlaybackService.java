e07d5b9dadcb3125f3770c36d0e5777cd530f7ca
Tyler Smith <tylerhesthedude@gmail.com>
2015-01-23 21:32:05 -0600

    improved icon, added the ability to jump back while paused

--- src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
+++ src/com/smithdtyler/prettygoodmusicplayer/MusicPlaybackService.java
@@ -469,10 +469,22 @@ public class MusicPlaybackService extends Service {
 			int progressMillis = mp.getCurrentPosition();
 			if (progressMillis <= 20000) {
 				mp.seekTo(0);
-				return;
 			} else {
 				mp.seekTo(progressMillis - 20000);
-				return;
+			}
+			lastPosition = mp.getCurrentPosition();
+		} else {
+			// if we're paused but initialized, try to seek
+			try{
+				int progressMillis = mp.getCurrentPosition();
+				if (progressMillis <= 20000) {
+					mp.seekTo(0);
+				} else {
+					mp.seekTo(progressMillis - 20000);
+				}
+				lastPosition = mp.getCurrentPosition();
+			} catch (Exception e){
+				Log.w(TAG, "Unable to seek to position, file may not have been loaded");
 			}
 		}
 		
