df26bdefd0ebc210d2bab48108850a886d5f40ab
Deve <deveee@gmail.com>
2015-02-03 19:19:46 +0100

    Fixed displaying "Choose Music Directory" dialog when text is translated

--- src/com/smithdtyler/prettygoodmusicplayer/SettingsActivity.java
+++ src/com/smithdtyler/prettygoodmusicplayer/SettingsActivity.java
@@ -103,7 +103,7 @@ public class SettingsActivity extends PreferenceActivity {
 			Preference preference) {
 		// TODO clean this up a bunch.
 		Log.i(TAG, "User clicked " + preference.getTitle());
-		if (preference.getTitle().equals("Choose Music Directory")) {
+		if (preference.getKey().equals("choose_music_directory_prompt")) {
 			final File path = Utils.getRootStorageDirectory();
 			DirectoryPickerOnClickListener picker = new DirectoryPickerOnClickListener(
 					this, path);
