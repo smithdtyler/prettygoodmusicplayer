eb47f23178465368127d3ff3487229bdcb2b559a
smith.d.tyler <smith.d.tyler@gmail.com>
2016-02-22 21:47:01 -0600

    handle empty base directory

--- src/com/smithdtyler/prettygoodmusicplayer/ArtistList.java
+++ src/com/smithdtyler/prettygoodmusicplayer/ArtistList.java
@@ -66,40 +66,44 @@ import java.util.Map;
 		}
 		
 		List<String> artistDirs = new ArrayList<String>();
-		artistDirs.add("All");
-		for(File dir : f.listFiles()){
-			if(Utils.isValidArtistDirectory(dir)){
-				artistDirs.add(dir.getName());
-			}
-		}
-		
-		Collections.sort(artistDirs, new Comparator<String>() {
 
-			@Override
-			public int compare(String arg0, String arg1) {
-				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ArtistList.this);
-				boolean ignoreleadingthe = sharedPref.getBoolean("ignore_leading_the_in_artist", false);
-				if (ignoreleadingthe) {
-					if (arg0.toLowerCase().startsWith("the ")) {
-						arg0 = arg0.substring(4);
-					}
-					if (arg1.toLowerCase().startsWith("the ")) {
-						arg1 = arg1.substring(4);
-					}
+		if(f.listFiles() != null) {
+			for (File dir : f.listFiles()) {
+				if (Utils.isValidArtistDirectory(dir)) {
+					artistDirs.add(dir.getName());
 				}
-				return (arg0.toUpperCase().compareTo(arg1.toUpperCase()));
 			}
 
-		});
-		
-		for(String artist : artistDirs){
-			Log.v(TAG, "Adding artist " + artist);
-			// listview requires a map
-			Map<String,String> map = new HashMap<String, String>();
-			map.put("artist", artist);			
-			artists.add(map);
+			Collections.sort(artistDirs, new Comparator<String>() {
+
+				@Override
+				public int compare(String arg0, String arg1) {
+					SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ArtistList.this);
+					boolean ignoreleadingthe = sharedPref.getBoolean("ignore_leading_the_in_artist", false);
+					if (ignoreleadingthe) {
+						if (arg0.toLowerCase().startsWith("the ")) {
+							arg0 = arg0.substring(4);
+						}
+						if (arg1.toLowerCase().startsWith("the ")) {
+							arg1 = arg1.substring(4);
+						}
+					}
+					return (arg0.toUpperCase().compareTo(arg1.toUpperCase()));
+				}
+
+			});
+
+            artistDirs.add(0,"All");
+
+			for (String artist : artistDirs) {
+				Log.v(TAG, "Adding artist " + artist);
+				// listview requires a map
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("artist", artist);
+				artists.add(map);
+			}
 		}
-		
+
 		if(!f.exists() || artistDirs.isEmpty()){
 			Map<String, String> map = new HashMap<String, String>();
 			map.put("artist", PICK_DIR_TEXT);
